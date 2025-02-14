0380acdc1019ae51ea8cf69499ec0d75084e82b7
sankar <sankusanku001@gmail.com>
2025-02-14 22:39:31 +0000

    basic style fix

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -67,7 +67,7 @@ export default function Login() {
         <form action="" onSubmit={(event) => handleSubmit(event)}>
           <div className="brand">
             <img src={Logo}  alt="logo" />
-            <h1>Emotract v1</h1>
+            <h1 className="text-red-200">Emotract v1</h1>
           </div>
           <input
             type="text"
